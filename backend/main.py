1df3f851926c394645a40372fc207612640f9728
Daniel Krueger <krugerdaniel2000@gmail.com>
2024-12-08 03:11:02 +0100

    Add Cors

--- backend/main.py
+++ backend/main.py
@@ -1,10 +1,19 @@
 import time
 
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 from agent import Agent
 
 app = FastAPI()
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+
+)
 
 agent = Agent()
 
