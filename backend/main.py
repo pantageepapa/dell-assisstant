a20fa103e11c4942708612100c9c834af3f67746
Daniel Krueger <krugerdaniel2000@gmail.com>
2024-12-08 04:07:35 +0100

    Add company data endpoint

--- backend/main.py
+++ backend/main.py
@@ -4,6 +4,7 @@ from fastapi import FastAPI, BackgroundTasks
 from fastapi.middleware.cors import CORSMiddleware
 
 from agent import Agent
+from backend.crawler.get_company_data import get_company_data
 
 app = FastAPI()
 app.add_middleware(
@@ -25,10 +26,11 @@ open('message_history.txt', 'w').close()
 @app.get("/chat/text")
 async def chat(message: str, background_tasks: BackgroundTasks):
     start = time.time()
-    response = agent.conversation_cycle(message)
+    response = agent.conversation_cycle(user_input=message)
     duration = time.time() - start
     background_tasks.add_task(agent.say, response)
     return {"response": response, "duration": duration}
+
 
 @app.get("/chat/mic")
 async def chat_mic(background_tasks: BackgroundTasks):
@@ -39,3 +41,8 @@ async def chat_mic(background_tasks: BackgroundTasks):
     return {"response": response, "duration": duration}
 
 
+@app.post("/company")
+async def company(company_name: str):
+    agent.company_name = company_name
+    agent.company = get_company_data(company_name)
+    return {"company_name": company_name, "company": agent.company}
